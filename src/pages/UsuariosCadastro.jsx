d51d7ce81d502d5c0f868a22bc30c8afb239e68e
Mauricio-Dolinski <mauriciompd@gmail.com>
2023-08-30 14:44:12 -0300

    fixed problems on user create

--- src/pages/UsuariosCadastro.jsx
+++ src/pages/UsuariosCadastro.jsx
@@ -29,7 +29,8 @@ const UsuariosCadastroPage = () => {
 	event.preventDefault();
     const data = new FormData(event.currentTarget);
     const entity = "usuarios";
-    
+    const params = new URLSearchParams(data);
+	
 	if (data){
 		if (toast.isActive(entity+"toast")){
 			toast.update(entity+"toast", {render: "Cadastrando...", type: "loading", isLoading: true, hideProgressBar: true, autoClose: false, closed: false});
@@ -39,15 +40,23 @@ const UsuariosCadastroPage = () => {
       			toastId: entity+"toast", closeButton: true, closeOnClick: true
     		});
 		}
-		
-	    await axios.post('http://localhost:8080/api/'+entity, {data}, {
+	    await axios.post('http://localhost:8080/api/'+entity, params.toString(), {
+			headers: {
+            	"content-type": "application/x-www-form-urlencoded"
+        	},
 			auth: {
 				username: user.user,
   				password: user.password
 			}
 		}).then(response => {
-	        toast.update(entity+"toast", {render: "Cadastrado", type: "success", isLoading: false, hideProgressBar: false, autoClose: 1200});
-    		navigate("/"+entity, { replace: true });
+			if (response.data === "Cadastrado"){
+				toast.update(entity+"toast", {render: response.data, type: "success", isLoading: false, hideProgressBar: false, autoClose: 1200});
+    			navigate("/"+entity, { replace: true });
+			}
+			else{
+				toast.update(entity+"toast", {render: response.data, type: "error", isLoading: false, hideProgressBar: false, autoClose: 3000});
+			}
+	        
 	      }).catch(error => { 
 			  if (error.response && error.response.status === 401){
 				    toast.update(entity+"toast", {render: "Acesso negado", type: "error", isLoading: false, hideProgressBar: false, autoClose: 3000});
@@ -62,13 +71,13 @@ const UsuariosCadastroPage = () => {
   return (
 	  <>
 	  	<Title name="Usuários - Cadastrar" />
-	  	<Box component="form" onSubmit={handleSubmit} gap="25px" noValidate sx={{ display: "flex", flexDirection: "column", m: "0px", p: "0px", alignItems: "flex-start" }}>
+	  	<Box component="form" onSubmit={handleSubmit} gap="25px" sx={{ display: "flex", flexDirection: "column", m: "0px", p: "0px", alignItems: "flex-start" }}>
 	  	  <Box sx={{ width: "100%", display: "flex", flexDirection: "row"}}>
 	  	  	<MyInput name="name" label="Nome Completo" color="success"/>
 	  	  	<MyInput name="email" label="Email" color="success"/>
 	  	  </Box>
 	  	  <Box sx={{ width: "100%", display: "flex", flexDirection: "row"}}>
-            <MyInput name="user" label="Usuário" color="success"/>
+            <MyInput name="username" label="Usuário" color="success"/>
             <MyInput name="role" label="Nivel de Acesso" color="success"/>
             <Box sx={{width: "100%", display: 'flex', alignItems: 'center',color: '#757575', marginX: '25px', p: 2, bgcolor: "#ffffff", borderRadius: 5, boxShadow: "2px 2px 10px -3px"}}>
 	          <Typography variant="h6" sx={{display: "flex", fontWeight: 'bold', marginRight: "25px"}}>
